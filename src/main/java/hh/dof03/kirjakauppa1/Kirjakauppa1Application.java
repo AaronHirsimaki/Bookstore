cc5a807aa73f61d4b41e8af3cfe4ffeeeb7072f7
Aaron Hirsimäki <hirsimakiaaronn@gmail.com>
2023-09-17 14:49:47 +0300

    vaihe1

--- src/main/java/hh/dof03/kirjakauppa1/Kirjakauppa1Application.java
+++ src/main/java/hh/dof03/kirjakauppa1/Kirjakauppa1Application.java
@@ -1,13 +1,27 @@
 package hh.dof03.kirjakauppa1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.dof03.kirjakauppa1.model.Book;
+import hh.dof03.kirjakauppa1.repository.BookRepository;
 
 @SpringBootApplication
 public class Kirjakauppa1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Kirjakauppa1Application.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+	return (args) -> {
+	repository.save(new Book("moi", "meikä", 2023, "620927", 500000.0));
+	repository.save(new Book("hei", "meikä", 2023, "730967", 800000.0));
+	repository.save(new Book("hehelol", "meikä", 2023, "126539", 900000.0));
+	};
 	}
 
 }
--- src/main/java/hh/dof03/kirjakauppa1/model/Book.java
+++ src/main/java/hh/dof03/kirjakauppa1/model/Book.java
@@ -1,11 +1,71 @@
 package hh.dof03.kirjakauppa1.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id; 
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int bookId;
     private String title;
     private String author;
+    @Column(name = "release_year")
     private int year;
     private String isbn;
-    private double price;
+    
+    public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	private double price;
 
     // Constructor
     public Book(String title, String author, int year, String isbn, double price) {
