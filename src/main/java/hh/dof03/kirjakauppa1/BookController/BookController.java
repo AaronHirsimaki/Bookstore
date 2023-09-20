05990bd448232a68d274d297d6826f1edd1afbc1
Aaron Hirsimäki <hirsimakiaaronn@gmail.com>
2023-09-20 19:14:34 +0300

    kirjakauppa edit ja delete

--- src/main/java/hh/dof03/kirjakauppa1/BookController/BookController.java
+++ src/main/java/hh/dof03/kirjakauppa1/BookController/BookController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,42 @@ import hh.dof03.kirjakauppa1.repository.BookRepository;
 
 @Controller
 public class BookController {
+	
+    @GetMapping("/addbook")
+    public String doAddBook(Model model) {
+    	Book book = new Book();
+    	model.addAttribute("book", book);
+        return "addBook";
+    }
+    
+    @PostMapping("/addbook")
+    public String doPostBook(Model model, @ModelAttribute("book") Book book) {
+    	bookRepository.save(book);
+    	model.addAttribute("booklist",bookRepository.findAll());
+    	return "booklist";
+    }
+    
+    @GetMapping("/deletebook/{id}")
+    public String deleteBookById(@PathVariable String id, Model model) {
+    	Book deleteBook = bookRepository.findById(Integer.parseInt(id));
+    	bookRepository.delete(deleteBook);
+    	model.addAttribute("booklist",bookRepository.findAll());
+    	return "booklist";
+    }
+    
+    @GetMapping("/editbook/{id}")
+    public String editBookById(@PathVariable String id, Model model) {
+    	Book editBook = bookRepository.findById(Integer.parseInt(id));
+    	model.addAttribute("book",editBook);
+    	return "editBook";
+    }
+    
+    @PostMapping("/editbook")
+    public String doPostEditBook(Model model, @ModelAttribute("book") Book book) {
+    	bookRepository.save(book);
+    	model.addAttribute("booklist",bookRepository.findAll());
+    	return "booklist";
+    }
 	
 	@Autowired
 	BookRepository bookRepository;
